bd9c7917fdebb8fc6ce0dff14d0f45fe9097fa88
David Arenas <davejs136@gmail.com>
2021-04-27 14:10:06 -0400

    add: basic server response

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('Hi');
+import app from './app';
+
+const domain: string = 'localhost';
+
+const serverReady = () =>
+  console.log(
+    `⚡ Server is running here 👉 http://${domain}:${app.get('port')}/`,
+  );
+
+app.listen(app.get('port'), serverReady);
