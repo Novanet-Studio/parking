4e370765c19e68f013d62ff3a6a8b5ab6148f823
David Arenas <davejs136@gmail.com>
2021-04-28 17:21:03 -0400

    feat: get user by id (test)

--- src/services/user/router.ts
+++ src/services/user/router.ts
@@ -4,5 +4,6 @@ import * as userController from './controller';
 const router = Router();
 
 router.get('/', userController.get);
+router.get('/:id', userController.getById);
 
 export default router;
