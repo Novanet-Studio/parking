e2fdc32aee9c11e92140109581641cdb165dc924
David Arenas <davejs136@gmail.com>
2021-04-28 19:45:19 -0400

    feat: add new user

--- src/services/user/controller.ts
+++ src/services/user/controller.ts
@@ -1,4 +1,5 @@
 import User from './model';
+import ServerResponse from '../../common/serverResponse';
 import type { Request, Response } from 'express';
 
 export const get = async (_: Request, res: Response): Promise<void> => {
@@ -11,4 +12,23 @@ export const getById = async (req: Request, res: Response): Promise<void> => {
 
   console.log(id);
   res.json({ message: 'Getting user by id' });
+};
+
+export const add = async (req: Request, res: Response): Promise<Response> => {
+  const server = new ServerResponse(res);
+  const exists = await User.findOne({ email: req.body.email });
+
+  if (exists) {
+    server.notFound(`User ${req.body.email} already exists`);
+  }
+
+  try {
+    const user = new User({ ...req.body });
+
+    await user.save();
+
+    return res.status(201).json('New user added');
+  } catch (error) {
+    return server.internalServerError(`Was an error creating user: ${error.message}`);
+  }
 };
