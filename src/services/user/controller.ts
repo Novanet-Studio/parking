9b90d9e334e8275bf77b653048a7c3ba5f5ee530
David Arenas <davejs136@gmail.com>
2021-04-28 17:21:03 -0400

    feat: get user by id (test)

--- src/services/user/controller.ts
+++ src/services/user/controller.ts
@@ -5,3 +5,10 @@ export const get = async (_: Request, res: Response): Promise<void> => {
   const users = await User.find();
   res.json(users);
 };
+
+export const getById = async (req: Request, res: Response): Promise<void> => {
+  const id = req.params.id;
+
+  console.log(id);
+  res.json({ message: 'Getting user by id' });
+};
