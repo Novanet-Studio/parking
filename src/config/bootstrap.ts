1e20a6369d69ae175acd2980a03037fda1fab1f9
David Arenas <davejs136@gmail.com>
2021-04-29 19:50:15 -0400

    add: createRoles fn

---
+++ src/config/bootstrap.ts
@@ -0,0 +1,19 @@
+import Role from '../services/role/model';
+
+export const createRoles = async (): Promise<void> => {
+  try {
+    const count = await Role.estimatedDocumentCount();
+
+    if (count > 0) return;
+
+    const values = await Promise.all([
+      new Role({ name: 'user' }).save(),
+      new Role({ name: 'moderator' }).save(),
+      new Role({ name: 'admin' }).save(),
+    ]);
+
+    console.log(values);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
