9467d10783a114ffe312f252894ec0476d996ae2
David Arenas <davejs136@gmail.com>
2021-04-29 19:49:43 -0400

    add: role service

---
+++ src/services/role/model.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from 'mongoose';
+import type { Document, Model } from 'mongoose';
+
+export interface RoleDocument extends Document {
+  name: string;
+}
+
+const roleSchema: Schema<RoleDocument> = new Schema(
+  {
+    name: String,
+  },
+  {
+    versionKey: false,
+  },
+);
+
+const Role: Model<RoleDocument> = model('Role', roleSchema);
+
+export default Role;
