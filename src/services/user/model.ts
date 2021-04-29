e5800de73d8d21c333e3f50ea712abecd1dac0f3
David Arenas <davejs136@gmail.com>
2021-04-29 19:52:03 -0400

    add: encryptPassword and comparePassword methods

--- src/services/user/model.ts
+++ src/services/user/model.ts
@@ -1,5 +1,6 @@
 import { model, Schema } from 'mongoose';
 import type { Document, Model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 export interface UserDocument extends Document {
   name: string;
@@ -26,11 +27,27 @@ const userSchema: Schema<UserDocument> = new Schema({
     type: String,
     required: true,
   },
-  apartmentNumber: Number,
-  seatsAllowed: Number,
-  reservedSeats: Number,
-  visitorBoothAssignment: Number,
+  // apartmentNumber: Number,
+  // seatsAllowed: Number,
+  // reservedSeats: Number,
+  // visitorBoothAssignment: Number,
+  roles: [{
+    ref: 'Role',
+    type: Schema.Types.ObjectId,
+  }]
+}, {
+  timestamps: true,
+  versionKey: false,
 });
+
+userSchema.methods.encryptPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+userSchema.methods.comparePassword = async (password: string, receivedPassword: string) => {
+  return bcrypt.compare(password, receivedPassword);
+};
 
 const User: Model<UserDocument> = model('User', userSchema);
 
